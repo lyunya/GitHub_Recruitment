8ea23e2c5eb02d4d62a1e2ee14edb60b4291b5c8
Leon Marbukh <leonmarbukh@gmail.com>
2020-03-17 16:52:50 -0700

    finished proj

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const searchURL = 'https://api.github.com/users/';
+
+
+function displayResults(responseJson) {
+  // if there are previous results, remove them
+  console.log(responseJson);
+  $('#results-list').empty();
+  // iterate through the articles array, stopping at the max number of results
+  for (let i = 0; i < responseJson.length; i++){
+    // for each video object in the articles
+    //array, add a list item to the results 
+    //list with the repo title and URL
+    $('#results-list').append(
+      `<li><h3><a href="${responseJson[i].html_url}">${responseJson[i].name}</a></h3>
+      </li>`
+    )};
+  //display the results section  
+  $('#results').removeClass('hidden');
+};
+
+function getRepos(name) {
+  
+  const url = searchURL + name + '/repos';
+
+  console.log(url);
+
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error(response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(err => {
+      $('#js-error-message').text(`Something went wrong: ${err.message}`);
+    });
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    const searchTerm = $('#js-search-term').val();
+    getRepos(searchTerm);
+  });
+}
+
+$(watchForm);
